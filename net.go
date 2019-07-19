e38ecd09952eb9782485c7e6aa443ce6f1e793c3
Aleksandr Razumov <ar@gortc.io>
2019-07-19 16:57:16 +0300

    net: init

---
+++ net.go
@@ -0,0 +1,85 @@
+package neo
+
+import (
+	"net"
+	"time"
+)
+
+// Net is virtual "net" package, implements mesh of peers.
+type Net struct {
+	peers map[string]*PacketConn
+}
+
+type packet struct {
+	buf  []byte
+	addr net.Addr
+}
+
+// PacketConn simulates mesh peer of Net.
+type PacketConn struct {
+	packets chan packet
+	addr    net.Addr
+	net     *Net
+}
+
+func (c *PacketConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	pp := <-c.packets
+	n = copy(p, pp.buf)
+	return n, pp.addr, nil
+}
+
+func (c *PacketConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
+	c.net.peers[addr.String()].packets <- packet{
+		addr: c.addr,
+		buf:  append([]byte{}, p...),
+	}
+	return len(p), nil
+}
+
+func (PacketConn) Close() error {
+	panic("implement me")
+}
+
+func (c PacketConn) LocalAddr() net.Addr {
+	return c.addr
+}
+
+func (PacketConn) SetDeadline(t time.Time) error {
+	panic("implement me")
+}
+
+func (PacketConn) SetReadDeadline(t time.Time) error {
+	panic("implement me")
+}
+
+func (PacketConn) SetWriteDeadline(t time.Time) error {
+	panic("implement me")
+}
+
+type NetAddr struct {
+	Net     string
+	Address string
+}
+
+func (n NetAddr) Network() string {
+	return n.Net
+}
+
+func (n NetAddr) String() string {
+	return n.Net + "/" + n.Address
+}
+
+func (n *Net) ListenPacket(network, address string) (net.PacketConn, error) {
+	a := &NetAddr{
+		// TODO: use virtual DNS.
+		Address: address,
+		Net:     network,
+	}
+	pc := &PacketConn{
+		net:     n,
+		addr:    a,
+		packets: make(chan packet, 10),
+	}
+	n.peers[a.String()] = pc
+	return pc, nil
+}
---
+++ net_test.go
@@ -0,0 +1,31 @@
+package neo
+
+import "testing"
+
+func TestNet_ListenPacket(t *testing.T) {
+	nt := &Net{
+		peers: make(map[string]*PacketConn),
+	}
+	left, err := nt.ListenPacket("v", "foo:123")
+	if err != nil {
+		t.Fatal(err)
+	}
+	right, err := nt.ListenPacket("v", "bar:123")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err = right.WriteTo([]byte("hello world"), left.LocalAddr()); err != nil {
+		t.Fatal(err)
+	}
+	buf := make([]byte, 1024)
+	n, addr, err := left.ReadFrom(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if addr.String() != "v/bar:123" {
+		t.Errorf("bad addr: %s", addr)
+	}
+	if string(buf[:n]) != "hello world" {
+		t.Errorf("bad message: %s", buf[:n])
+	}
+}
