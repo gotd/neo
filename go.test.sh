e47bb3096bbfb928490e13a9317eecb2e82b4079
Aleksandr Razumov <ar@gortc.io>
2019-07-19 14:29:16 +0300

    ci: update go.test.sh

--- go.test.sh
+++ go.test.sh
@@ -6,12 +6,9 @@ touch coverage.txt
 # quick-test without -race
 go test ./...
 
-# quick-test with "debug"
-go test -tags debug ./...
-
 for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic "$d"
-    if [ -f profile.out ]; then
+    if [[ -f profile.out ]]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
